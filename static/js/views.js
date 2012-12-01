41bf02dd24773d2cb4e1dbf3564ea924895e119d
Augustin Riedinger <augustin.riedinger@gmail.com>
2012-12-02 00:55:19 +0100

    ok sur le fil

--- static/js/views.js
+++ static/js/views.js
@@ -8,3 +8,51 @@ window.MenuView = Backbone.View.extend({
 		return this;
 	}
 });
+
+window.LoadingView = Backbone.View.extend({
+	className : 'loading',
+	render : function(eventName) {
+		$(this.el).html('<img src="img/ajax-loader.gif" />');
+		return this;
+	}
+});
+
+window.ServiceView = Backbone.View.extend({
+	className : 'service-block',
+	template : _.template($('#tpl-service').html()),
+	initialize : function() {
+	},
+	render : function(eventName) {
+		if (this.model) {
+			$(this.el).html(this.template(this.model.toJSON()));
+		} else {
+			$(this.el).html(new LoadingView.render().el);
+		}
+		return this;
+	}
+});
+
+window.ServicesView = Backbone.View.extend({
+	tagName : 'tbody',
+	initialize : function() {
+		this.collection.on('reset',this.render,this);
+		this.collection.view = this;
+	},
+	render : function(eventName) {
+		$(this.el).empty();
+		if (this.collection.length) {
+			_.each(this.collection.models,function(service) {
+				var view = new ServiceView({
+					model : service
+				});
+				$(this.el).append('<tr><td class="service"></td></tr>');
+				$(this.el).find('.service').append(view.render().el);
+			},this);
+		} else {
+			$(this.el).html('<tr><td class="service"></td></tr>');
+			$(this.el).find('.service').html(new LoadingView({}).render().el);
+			
+		}
+		return this;
+	}
+});
