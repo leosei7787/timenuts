e5cd19d0956fad804e02f11c291b9e936ee854d3
Leo Sei <sei7787@gmail.com>
2012-12-01 16:15:24 +0100

    delete routeur

--- server/Routeur.py
+++
@@ -1,7 +0,0 @@
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
-
-class services(webapp.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
