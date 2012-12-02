7a1c9579359ade09ac7ef3cc27cfc02414476812
Augustin Riedinger <augustin.riedinger@gmail.com>
2012-12-02 08:07:48 +0100

    my config

--- server/config.py
+++ server/config.py
@@ -68,9 +68,25 @@ ConfigProfils = [
         "Headline" : ":P",
 		"Skills":[
 			"Web Application",
-			"French",
+			"French"
 		]
 	},
+	{
+		"Email":"augustin.riedinger@gmail.com",
+		"FirstName" : "Augustin",
+		"LastName" : "Riedinger",
+		"Address" : "21 rue servan, 75011 Paris",
+        "Image": "http://en.gravatar.com/avatar/bc22b26eca68dab55ec4fe491495951e",
+		"Involvement":450,
+		"TimeCredit":24,
+        "Headline" : "I <3 Hackathons",
+		"Skills":[
+			"Web Application",
+			"French",
+			"House Keeping",
+			"English"
+		]
+	}
 ]
 
 ConfigServices = [
